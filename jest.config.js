76cf0a9f60670702dc7f478bf112da020102dea9
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-03-04 16:27:38 -0300

    fix: add . in jest configuration transform

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'jest-environment-node',
   transform: {
-    '.+\\ts$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest'
   }
 }
