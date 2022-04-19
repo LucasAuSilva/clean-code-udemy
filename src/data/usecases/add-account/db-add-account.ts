356603f38e7a783423e79e6412864cfa70c20a0b
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-04-19 16:45:33 -0300

    feat: ensure DbAddAccount retuns an account on sucess

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -11,7 +11,6 @@ export class DbAddAccount implements AddAccount {
 
   async add (account: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(account.password)
-    await this.addAccountRepository.add(Object.assign({}, account, { password: hashedPassword }))
-    return Promise.resolve(null)
+    return this.addAccountRepository.add(Object.assign({}, account, { password: hashedPassword }))
   }
 }
