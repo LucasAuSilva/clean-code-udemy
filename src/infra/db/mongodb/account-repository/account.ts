6de92c36088cf979f844c2809b811edc4eda2ebd
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-04-20 14:37:32 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,16 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(account)
+    const newAccount = await accountCollection.findOne({ _id: result.insertedId })
+    return {
+      id: newAccount._id.toHexString(),
+      ...account
+    }
+  }
+}
