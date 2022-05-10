fe67cd6b59631b0c9474a7d6b1f229b8bb6d4d0f
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-05-10 12:08:08 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from '../../presentation/controllers/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  return new SignUpController(
+    new EmailValidatorAdapter(),
+    new DbAddAccount(
+      new BcryptAdapter(salt),
+      new AccountMongoRepository()
+    )
+  )
+}
