17c243cd8d4665097a2f2ce8bb8529d69acb645e
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-06-15 17:02:19 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/data/protocols/index.ts
+++ src/data/protocols/index.ts
@@ -3,3 +3,4 @@ export * from './db/load-account-by-email-repository'
 export * from './db/log-error-repository'
 
 export * from './criptography/encrypter'
+export * from './criptography/hash-comparer'
