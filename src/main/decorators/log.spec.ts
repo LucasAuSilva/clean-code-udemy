a48dc001c6c0aeb150bc830fbb8f25414aaf59e0
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-06-15 10:31:38 -0300

    refactor: move http-helper to a folder

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,4 +1,4 @@
-import { ok, serverError } from '../../presentation/helpers/http-helper'
+import { ok, serverError } from '../../presentation/helpers/http/http-helper'
 import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { Validation, Controller, HttpResponse, HttpRequest, Authentication } from './login-protocols'
-import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
 
 export class LoginController implements Controller {
   private readonly authentication: Authentication
