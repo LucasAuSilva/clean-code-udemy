c8e850a7b9ec266be47068b4146572e29831794c
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-06-21 13:41:28 -0300

    test: ensure AuthMiddleware retruns 403 if no x-access-token exists in headers

---
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,10 @@
+import { AccessDeniedError, forbidden } from './auth-middleware-protocols'
+import { AuthMiddleware } from './auth-middleware'
+
+describe('Auth Middleware', () => {
+  test('Should return 403 if no x-access-token exists in headers', async () => {
+    const sut = new AuthMiddleware()
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+})
