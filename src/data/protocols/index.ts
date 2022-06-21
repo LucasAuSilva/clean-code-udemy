9828c94e5a356ce3348096264f1600399fa0af9f
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-06-21 10:40:58 -0300

    feat: ensure DbAddSurvey calls AddSurveyRepository with correct values

--- src/data/protocols/index.ts
+++ src/data/protocols/index.ts
@@ -3,6 +3,8 @@ export * from './db/account/load-account-by-email-repository'
 export * from './db/log/log-error-repository'
 export * from './db/log/update-access-token-repository'
 
+export * from './db/survey/add-survey-repository'
+
 export * from './criptography/hasher'
 export * from './criptography/hash-comparer'
 export * from './criptography/encrypter'
