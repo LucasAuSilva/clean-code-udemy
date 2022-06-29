e32f5f7e4628752520ee1e1901d77d7e83df1783
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-06-29 12:42:20 -0300

    feat: add Swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'clean-code-udemy',
+    description: 'Project develop in Node.JS for explain advanced concepts in programming like TDD, Clean Architecture, Design Patterns and SOLID',
+    version: '1.0.0'
+  }
+}
