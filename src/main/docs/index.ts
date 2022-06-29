5d5d9a1dcb69409634a2d28b52daaa33e4796b41
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-06-29 15:36:59 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamSchema } from './schemas/login-param-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
-    title: 'clean-code-udemy',
+    title: 'Clean Code Udemy',
     description: 'Project develop in Node.JS for explain advanced concepts in programming like TDD, Clean Architecture, Design Patterns and SOLID',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Account'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamSchema
   }
 }
---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,27 @@
+export const loginPath = {
+  post: {
+    tags: ['Account'],
+    summary: 'API for authenticate the user',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      }
+    }
+  }
+}
