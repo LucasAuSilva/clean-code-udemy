de940c8d01e80253480399c10f7813c86550cb7d
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-06-29 00:27:33 -0300

    refactor: save accountId and surveyId like ObjectId instead of string

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -2,13 +2,14 @@ import { SurveyResultModel } from '@/domain/models/survey-result'
 import { SaveSurveyResultModel } from '@/domain/usecases/survey-result/save-survey-result'
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
     const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
     const surveyResult = await surveyResultCollection.findOneAndUpdate({
-      surveyId: data.surveyId,
-      accountId: data.accountId
+      surveyId: new ObjectId(data.surveyId),
+      accountId: new ObjectId(data.accountId)
     }, {
       $set: {
         answer: data.answer,
