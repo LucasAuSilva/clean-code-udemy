abdc5ad9fe7f5c34c749b3484ef7d8262cecae9f
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-05 21:52:59 -0300

    feat: setup apollo server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,14 @@
-import express from 'express'
+import express, { Express } from 'express'
+import setupApolloServer from './apollo-server'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
-import setupSwagger from './config-swagger'
+import setupSwagger from './swagger'
 
-const app = express()
-setupSwagger(app)
-setupMiddlewares(app)
-setupRoutes(app)
-
-export default app
+export const setupApp = async (): Promise<Express> => {
+  const app = express()
+  await setupApolloServer(app)
+  setupSwagger(app)
+  setupMiddlewares(app)
+  setupRoutes(app)
+  return app
+}
