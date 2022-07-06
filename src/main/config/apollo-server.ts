b5d2c2df88ad055182a6514bb5de61d78b8c79bd
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-06 00:14:43 -0300

    feat: add auth directives to surveys query

--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -1,5 +1,7 @@
 import resolvers from '@/main/graphql/resolvers'
 import typeDefs from '@/main/graphql/type-defs'
+import { authDirectiveTransformer } from '@/main/graphql/directives'
+import { makeExecutableSchema } from '@graphql-tools/schema'
 import { ApolloServer } from 'apollo-server-express'
 import { Express } from 'express'
 import { GraphQLError } from 'graphql'
@@ -23,10 +25,15 @@ const checkError = (error: GraphQLError, errorName: string): boolean => {
   return [error.name, error.originalError?.name].some(name => name === errorName)
 }
 
+let schema = makeExecutableSchema({ resolvers, typeDefs })
+schema = authDirectiveTransformer(schema)
+
 export default async (app: Express): Promise<void> => {
   const server = new ApolloServer({
+    schema,
     resolvers,
     typeDefs,
+    context: ({ req }) => ({ req }),
     plugins: [{
       requestDidStart: async () => ({
         willSendResponse: async ({ response, errors }) => handleErrors(response, errors)
